bbd9b4a596e44a61e03ba7c6c5dae05b13869808
Murilo Cabreira <murilocabreira@hotmail.com>
2023-05-12 12:52:02 -0300

    feat: add button and delete button was created and  automatiautomatically generating domains

--- client/src/App.vue
+++ client/src/App.vue
@@ -17,58 +17,96 @@
         <div class="row">
           <div class="col-md">
             <h5>
-              Prefixos <span class="badge bg-info">{{ prefixes.length }}</span>
+              Prefixos
+              <span class="badge bg-primary">{{ prefixes.length }}</span>
             </h5>
 
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
                   <li
-                    class="list-group-item"
+                    class="list-group-item d-flex justify-content-between"
                     v-for="prefix in prefixes"
                     :key="prefix"
                   >
                     {{ prefix }}
+                    <button
+                      class="mt-1 material-symbols-outlined badge bg-danger"
+                      @click="deletePrefix(prefix)"
+                    >
+                      delete
+                    </button>
                   </li>
                 </ul>
                 <br />
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Digite o prefixo"
-                />
+                <div class="input-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Digite o prefixo"
+                    v-model="prefix"
+                    @keyup.enter="addPrefix(prefix)"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" @click="addPrefix(prefix)">
+                      <span class="pt-1 text-material-symbols-outlined">
+                        add
+                      </span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div class="col-md">
             <h5>
-              Sufixos <span class="badge bg-info">{{ sufixes.length }}</span>
+              Sufixos <span class="badge bg-primary">{{ sufixes.length }}</span>
             </h5>
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
                   <li
-                    class="list-group-item"
+                    class="list-group-item d-flex justify-content-between"
                     v-for="sufix in sufixes"
                     :key="sufix"
                   >
                     {{ sufix }}
+
+                    <button
+                      class="mt-1 material-symbols-outlined badge bg-danger"
+                      @click="deleteSufix(sufix)"
+                    >
+                      delete
+                    </button>
                   </li>
                 </ul>
                 <br />
-                <input
-                  type="text"
-                  class="form-control"
-                  placeholder="Digite o prefixo"
-                />
+                <div class="input-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Digite o sufixo"
+                    v-model="sufix"
+                    @keyup.enter="addSufix(sufix)"
+                  />
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" @click="addSufix(sufix)">
+                      <span class="pt-1 text-material-symbols-outlined">
+                        add
+                      </span>
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
         <br />
-        <h5>getName(s) <span class="badge bg-info">0</span></h5>
+        <h5>
+          getName(s) <span class="badge bg-primary">{{ domains.length }}</span>
+        </h5>
         <div class="card">
-          <div class="card-body">
+          <div class="mt-3 card-body">
             <ul class="list-group">
               <li
                 class="list-group-item"
@@ -79,11 +117,6 @@
               </li>
             </ul>
             <br />
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Digite o prefixo"
-            />
           </div>
         </div>
       </div>
@@ -97,10 +130,41 @@ export default {
   name: 'app',
   data: () => {
     return {
-      prefixes: ['Moon', 'Ride', 'Walk'],
-      sufixes: ['Forest', 'Hub', 'Trip'],
-      domains: ['MoonForest', 'MoonHub', 'MoonTrip'],
+      prefix: '',
+      sufix: '',
+      prefixes: ['Moon', 'Ride'],
+      sufixes: ['Forest', 'Hub'],
+      domains: [],
     };
+  },
+  methods: {
+    addPrefix(prefix) {
+      this.prefixes.push(prefix);
+      this.prefix = '';
+      this.generate();
+    },
+    deletePrefix(prefix) {
+      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+      this.generate();
+    },
+    addSufix(sufix) {
+      this.sufixes.push(sufix);
+      this.sufix = '';
+      this.generate();
+    },
+    deleteSufix(sufix) {
+      this.sufixes.splice(this.prefixes.indexOf(sufix), 1);
+      this.generate();
+    },
+    generate() {
+      this.domains = [];
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          this.domains.push(prefix + sufix);
+          this.domains.push(sufix + prefix);
+        }
+      }
+    },
   },
 };
 </script>
